eca6843560f4ba4a7eb14cde8a884b15cf5187ca
Albin <albinronnkvist@protonmail.com>
2024-09-06 21:59:52 +0200

    Adjust DI in Jobs.Initializer

--- HybridSearch/Jobs.Initializer/Program.cs
+++ HybridSearch/Jobs.Initializer/Program.cs
@@ -2,9 +2,7 @@ using AlbinRonnkvist.HybridSearch.Jobs.Initializer;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<ProductsInitializer>();
-builder.Services.ConfigureElasticsearch(builder.Configuration, builder.Environment);
-builder.Services.ConfigureCustomServices();
-builder.Services.ConfigureIndices(builder.Configuration);
+builder.Services.ConfigureJobsInitializerProject(builder.Configuration, builder.Environment);
 
 var host = builder.Build();
 host.Run();
