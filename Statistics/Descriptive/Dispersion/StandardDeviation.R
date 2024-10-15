873946fac4dbcac39307d0176fd89bc5f87b8da9
Albin <albinronnkvist@protonmail.com>
2024-10-15 12:37:17 +0200

    Add Dispersion and Standard Deviation

---
+++ Statistics/Descriptive/Dispersion/StandardDeviation.R
@@ -0,0 +1,11 @@
+ages <- c(25, 30, 28, 24, 32, 26, 29, 31, 27)
+
+# Calculate the mean age
+mean_age <- mean(ages)
+
+# Calculate the standard deviation of ages
+std_dev_age <- sd(ages)
+
+# Output the results
+mean_age
+std_dev_age
