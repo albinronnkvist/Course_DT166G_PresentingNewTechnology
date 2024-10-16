6112bbebeb235df5e044774d89d7d92ffd5e3ac8
Albin <albinronnkvist@protonmail.com>
2024-10-16 11:19:38 +0200

    Add Median

---
+++ Statistics/Descriptive/CentralTendency/Median.R
@@ -0,0 +1,17 @@
+# Odd number of items in data set
+# Skewed data set
+data_odd <- c(10, 12, 14, 15, 100)
+
+# Calculate the median
+odd_median_value <- median(data_odd)
+
+odd_median_value
+
+
+
+# Even number of items in data set
+data_even <- c(4, 8, 6, 5, 3, 7)
+
+even_median_value <- median(data_even)
+
+even_median_value
