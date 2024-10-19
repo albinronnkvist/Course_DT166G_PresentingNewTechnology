bb9090ea39dcf42c6ba25e4e410331695c48b26f
Albin <albinronnkvist@protonmail.com>
2024-10-19 19:01:14 +0200

    Add more tests

--- Statistics/Tests/ShapiroWilkTest.R
+++ Statistics/Tests/ShapiroWilkTest.R
@@ -5,3 +5,8 @@ shapiro.test(data_normal)
 # Example 2
 data_non_normal <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 100)
 shapiro.test(data_non_normal)
+
+data_small_normal <- c(48, 50, 52, 49, 51, 50)
+
+# Perform Shapiro-Wilk test to check for normality
+shapiro.test(data_small_normal)
