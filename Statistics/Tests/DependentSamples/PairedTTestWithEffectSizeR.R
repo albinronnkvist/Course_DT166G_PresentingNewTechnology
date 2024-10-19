d587803c2788725bd0f34d377403b7514b064046
Albin <albinronnkvist@protonmail.com>
2024-10-19 22:11:49 +0200

    Add effect size r examples for parametric tests

---
+++ Statistics/Tests/DependentSamples/PairedTTestWithEffectSizeR.R
@@ -0,0 +1,14 @@
+pre_test <- c(50, 55, 60, 62, 70, 72, 75)
+post_test <- c(52, 58, 63, 66, 72, 76, 80)
+
+t_test <- t.test(pre_test, post_test, paired = TRUE)
+
+t_value <- t_test$statistic
+df <- t_test$parameter
+
+# Calculate effect size r
+r <- t_value / sqrt(t_value^2 + df)
+
+cat("t-value:", t_value, "\n")
+cat("Degrees of freedom (df):", df, "\n")
+cat("Effect size (r):", r, "\n")
---
+++ Statistics/Tests/IndependentSamples/TTestWithEffectSizeR.R
@@ -0,0 +1,14 @@
+group1 <- c(50, 52, 54, 56, 58, 60, 62)
+group2 <- c(65, 67, 70, 72, 74, 75, 77)
+
+t_test <- t.test(group1, group2)
+
+t_value <- t_test$statistic
+df <- t_test$parameter
+
+# Calculate effect size r
+r <- t_value / sqrt(t_value^2 + df)
+
+cat("t-value:", t_value, "\n")
+cat("Degrees of freedom (df):", df, "\n")
+cat("Effect size (r):", r, "\n")
